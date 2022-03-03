63fe4f09312682b7f8b15ea3ae3de7001cbbfa3a
jostenfernandes <jostenfernandes865@gmail.com>
2022-03-03 09:32:38 +0530

    First Commit

---
+++ miniproject/src/com/lti/miniproject/CsvFileReader.java
@@ -0,0 +1,57 @@
+package com.lti.miniproject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+public class CsvFileReader{
+	HashMap <String, Complaints> MapBankobjcompdet = new HashMap<>();
+	public void fileLoader() {
+	 String path="C:\\Users\\JOSTEN FERNANDES\\Desktop\\complaints (1).csv";
+	 String line="";
+	 
+	 try {
+		BufferedReader bf=new BufferedReader(new FileReader(path));
+		while ((line=bf.readLine())!=null)
+		{   
+			
+			//String[] elements=line.split(",(?=(?:[^\"]\"[^\"]\")[^\"]$)", -1);
+			String[] elements=line.split(",");
+			Complaints objcompdet=new Complaints();
+			objcompdet.dateReceived=elements[0];
+			objcompdet.product=elements[1];
+			objcompdet.subProduct=elements[2];
+			objcompdet.issue=elements[3];
+			objcompdet.subIssue=elements[4];
+			objcompdet.company=elements[5];
+			objcompdet.state=elements[6];
+			objcompdet.ZIPcode=elements[7];
+			objcompdet.submittedVia=elements[8];
+			objcompdet.dateSentToCompany=elements[9];
+			objcompdet.companyResponseToConsumer=elements[10];
+			objcompdet.timelyResponse=elements[11];
+			objcompdet.consumerDisputed=elements[12];
+
+			
+			MapBankobjcompdet.put(elements[13],objcompdet); 
+		}
+		bf.close();
+		
+	} catch (FileNotFoundException e) {
+		
+		e.printStackTrace();
+	} catch (IOException e) {
+		
+		e.printStackTrace();
+	}
+	 
+	 
+	}
+
+	HashMap<String,Complaints> GetMapBankobjcompdet(){
+		
+		return MapBankobjcompdet;
+		
+	}
+}
